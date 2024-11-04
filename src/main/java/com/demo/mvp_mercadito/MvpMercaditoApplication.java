b158ce525760418890ae89d0274c3c5aa39b0bbc
NoeliaAlbornoz <albornoznoeliaunlam@gmail.com>
2024-11-04 16:50:57 -0300

    Inicializar el proyecto con archivos base

---
+++ src/main/java/com/demo/mvp_mercadito/MvpMercaditoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mvp_mercadito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvpMercaditoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvpMercaditoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mvp_mercadito/MvpMercaditoApplicationTests.java
@@ -0,0 +1,8 @@
+package com.demo.mvp_mercadito;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpMercaditoApplicationTests {
+
+}
